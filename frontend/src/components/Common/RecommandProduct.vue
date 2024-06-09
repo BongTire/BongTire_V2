2272c4a02e8b7177f91e88473724bc9f577970a2
진민 <insomnis2020@naver.com>
2024-06-09 23:44:06 +0900

    url 및 클릭 이벤츠 추가

--- frontend/src/components/Common/RecommandProduct.vue
+++ frontend/src/components/Common/RecommandProduct.vue
@@ -9,11 +9,12 @@ const props = defineProps({
 })
 
 const clickProductListPage = () =>{
-
+  router.push('/tire?pccd=P0601')
 }
 
-const clickProductPage = () =>{
-
+const clickProductPage = (product:IProduct) =>{
+  console.log(product)
+  router.push(`/product/${product.TireId}?pccd=${product.PCCD}`)
 }
 
 
@@ -29,20 +30,20 @@ const clickProductPage = () =>{
           <span aria-hidden="true"> &rarr;</span>
         </p>
       </div>
-      <div class="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4 overflow-y-auto h-256">
+      <div class="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4 overflow-y-auto h-72">
         <div v-for="product in props.products" :key="product.id" class="group relative">
           <div class="aspect-h-3 aspect-w-4 overflow-hidden rounded-lg bg-gray-100">
             <img :src="product.image" :alt="product.image" class="h-full w-auto m-auto " />
-            <div class="flex items-end p-4 opacity-0 group-hover:opacity-100" aria-hidden="true">
-              <div @click="clickProductPage" class="w-full rounded-md bg-white bg-opacity-75 px-4 py-2 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter">상품 더 보기</div>
+            <div @click="clickProductPage(product)" class="flex items-end p-4 opacity-0 group-hover:opacity-100" aria-hidden="true">
+              <div  class="w-full rounded-md bg-white bg-opacity-75 px-4 py-2 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter">상품 더 보기</div>
             </div>
           </div>
           <div class="mt-4 flex items-center justify-between space-x-8 text-base font-medium text-gray-900">
-            <h3>
-              <a href="#">
+            <h3 >
+              <router-link :to="`/product/${product.TireId}?pccd=P0601`">
                 <span aria-hidden="true" class="absolute inset-0" />
                 {{ product.productName }}
-              </a>
+              </router-link>
             </h3>
             <p>{{ product.discountPrice }}</p>
           </div>
